c829c2626efee75c54dc9616c2561b4c62417187
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-05-04 16:01:44 -0300

    u´date

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,13 @@ const mobile_nav = document.querySelector('.mobile-nav')
 
 
 
-function Abrir_Menu_Mobile(){
+function Abrir_Menu_Mobile() {
     mobile_nav.style.marginTop = '0'
 }
-function Fechar_Menu_Mobile(){
+function Fechar_Menu_Mobile() {
     mobile_nav.style.marginTop = '-100vh'
 }
+
+
+const InjetarTexo = document.querySelector('#injetar_texto').innerHTML =
+   'Olá Meu nome é Cauê Atualmente sou estagiario em Front-End construindo paginas em Html Css e Javascript, tambem Administração de sites com o Wordpress,pretendo em especializar no React e Talvez em Node.js tambem tenho interesses na area mobile mais ainda não comecei os estudos nela '
