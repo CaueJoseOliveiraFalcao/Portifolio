8ed62c6c17a176d82b5f6880bff5e5be1cf5ae55
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-05-04 13:54:05 -0300

    menu mobule

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const hamburguer = document.querySelector('.hambuerguer')
+const mobile_nav = document.querySelector('.mobile-nav')
+
+
+
+function Abrir_Menu_Mobile(){
+    mobile_nav.style.marginTop = '0'
+}
+function Fechar_Menu_Mobile(){
+    mobile_nav.style.marginTop = '-100vh'
+}
