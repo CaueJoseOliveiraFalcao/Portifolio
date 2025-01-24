1711492be0fa3525879a009681b5db5d4f6ccaff
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2025-01-24 11:37:55 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,15 +12,13 @@ function Fechar_Menu_Mobile() {
 
 
 const InjetarTexo = document.querySelector('#injetar_texto').innerHTML =
-   'Olá Meu nome é Cauê Atualmente sou estagiario em Front-End construindo paginas em Html Css e Javascript, tambem Administração de sites com o Wordpress,pretendo em especializar no React e Talvez em Node.js tambem tenho interesses na area mobile mais ainda não comecei os estudos nela '
+   'Sou um desenvolvedor com experiência prática e foco em criar soluções eficientes para a web. Meu aprendizado veio principalmente de projetos reais, onde pude aprimorar minhas habilidades e enfrentar desafios do mundo da tecnologia. Tenho conhecimento em desenvolvimento backend e frontend, além de experiência com infraestrutura de servidores e cloud computing. Busco sempre expandir meu conhecimento e aplicar as melhores práticas para garantir desempenho, segurança e escalabilidade nos projetos em que trabalho.'
 
 const cards = [
-    {img : 'assets/img/vscode-icons_file-type-html.png'},
-    {img : 'assets/img/vscode-icons_file-type-css.png'},
-    {img : 'assets/img/logos_javascript.png'},
-    {img : 'assets/img/logos_bootstrap.png'},
-    {img : 'assets/img/WordPress-logotype-wmark.png'},
-    {img : 'assets/img/jquery-logo.png'}
+    {img : 'assets/img/laravel.png'},
+    {img : 'assets/img/linux.png'},
+    {img : 'assets/img/mysql.png'},
+    {img : 'assets/img/next.jpeg'},
 
 ]
 for (i = 0; i < cards.length; i++){
